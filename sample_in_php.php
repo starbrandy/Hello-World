e04212284e83e1708271235a6204d2479a465d41
starbrandy <brussum-cw@starbucks.com>
2013-09-18 16:21:01 -0700

    Update sample_in_php.php

--- sample_in_php.php
+++ sample_in_php.php
@@ -1,50 +1,36 @@
-<?php
-function cURLcheckBasicFunctions() 
-{ 
-  if( !function_exists("curl_init") && 
-      !function_exists("curl_setopt") && 
-      !function_exists("curl_exec") && 
-      !function_exists("curl_close") ) return false; 
-  else return true; 
-} 
+<?
+$path = "/YOUR/PATH/TO/public_html/php_diary/data";
+$filename = "122499.dat";
+$x= -1;
+  if($file = fopen("$path/$filename", "r"))
+  {
+    while(!feof($file))
+    {
+      $therate = fgetss($file, 255);
+      $x++;
+      $count = $count + $therate;
+    }
+    fclose($file);
+  }
+$average = ($count / $x);
+print("Surfer Average Rating for 12/24/99: ");
+printf("%.2f", $average);
+print("<br>Total Surfer Votes: $x");
+?>
 
-// declare
-if( !cURLcheckBasicFunctions() ) print_r('UNAVAILABLE: cURL Basic Functions');
-$apikey = '';
-$clientId = '';
-$clientSecret = '';
-$url = 'https://test.openapi.starbucks.com/v1/oauth/token?api_key='.$apikey;
-$ch = curl_init($url);
-$fields = array(
-'grant_type' => urlencode('client_credentials'),
-'client_id' => urlencode($clientId),
-'client_secret' => urlencode($clientSecret),
-'scope' => urlencode('test_scope'),
-'state' => urlencode('some_state'));
-
-//url-ify the data for the POST
-$fields_string = '';
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string, '&');	
-curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC ) ; 
-// curl_setopt($ch, CURLOPT_USERPWD, $credentials);
-// curl_setopt($ch, CURLOPT_SSLVERSION, 3); 
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);			
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_POST, count($fields));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-
-//execute post
-if(curl_exec($ch) === false)
-{
-    echo 'Curl error: ' . curl_error($ch);
+<?
+$diary_entries = array('121799','121899','121999','122199','122099','122399','122299',
+'122699','122499','122799','122899','123099','122999','010800','123199','010100',
+'010200','010500','010300','010700','010600','010900','011000','012100','011400',
+'070700','012200','021500','071400','070800','071200','071300','073000','072000',
+'072900','072800','073100','021801','021901','122802','122902','030301','030401',
+'042701');
+sort($diary_entries);
+foreach ($diary_entries as $s) {
+  $month = substr($s, 0, 2);
+  $day = substr($s, 2, 2);
+  $year = substr($s, 4, 2);
+  $unix_timestamp = mktime(0,0,0, $month, $day, $year);
+  print("$s = $unix_timestamp<br>");
 }
-else
-{
-    echo 'Operation completed without any errors';
-}
-
-//close connection
-curl_close($ch);
-?> 
+?>
